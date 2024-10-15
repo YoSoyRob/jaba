32dfa59ada2e35852aa9b6e7237a76c6735931a1
Rob <rob.mtzgu@gmail.com>
2024-10-14 21:14:08 -0600

    primer

---
+++ tipos-de-dato.js
@@ -0,0 +1,47 @@
+//42
+
+console.log (typeof 42)
+
+//"veinticinco"
+
+console.log(typeof "veinticinco")
+
+//-666
+
+console.log(typeof -666)
+
+console.log((1) + (-667))
+
+//true
+
+console.log(typeof true !== true)
+
+//0
+
+console.log(typeof 0 )
+
+console.log(parseFloat (0)+ parseFloat (0)  )
+
+//''
+
+let mensaje = " "
+
+console.log(mensaje)
+
+console.log(typeof mensaje)
+
+//undefined
+
+let miMensaje
+
+console.log(miMensaje)
+
+//null
+
+miMensaje = null
+
+console.log(miMensaje)
+
+//false
+
+console.log(typeof 10 == 10)
