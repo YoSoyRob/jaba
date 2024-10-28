5d6057b702de9dc31e1461b1fc91bd3e8e5a9850
Rob <rob.mtzgu@gmail.com>
2024-10-27 19:48:01 -0600

    arreglos

--- clase4/main.js
+++ clase4/main.js
@@ -38,10 +38,56 @@ for (let i= 0; i < numeros.length; i++){
 console.log (frutas)
 
 for (let i = 0; i < frutas.length; i++) {
-    console.log(frutas[i].toUpperCase())
+    console.log(frutas[i].toUpperCase()) //al poner un punto (.) indicara los metodos disponibles de acuerdo al tipo de dato
 }
 
-let miFrutaFavorita = frutas.indexOf("mango") /*muestra la posicion del elemento buscado, si no existe, arroja un -1, 
-.toLowerCase convierte una cadena en minusculas .toUpperCase, convierte todas a mayusculas aun que se encuentren guardadas en minusculas */
+let miFrutaFavorita = frutas.indexOf("Mango".toLowerCase()) /*muestra la posicion del elemento buscado, si no existe, arroja un -1, 
+.toLowerCase convierte una cadena en minusculas .toUpperCase, convierte todas a mayusculas aun que se encuentren guardadas en minusculas,
+al usar el .toLowerCase se debe de poner () al final*/
 
 console.log(miFrutaFavorita)
+
+console.log (frutas[miFrutaFavorita])
+
+numeros.push(-3);
+
+frutas.unshift("sandia") //en el ejemplo fue "pera"
+
+console.log(numeros)
+
+console.log(frutas)
+
+const pasatiempos = "videojuegos guitarra gym musica";
+
+const arregloPasatiempos = pasatiempos.split(" "); /*el valor que se señale aqui, es el que considerara para separar 
+los valores dados cuando los encuentre */
+
+//const pasatiemposUnidos = arregloPasatiempos.join() //une los valores
+
+console.log(arregloPasatiempos)
+
+const nuevosPasatiempos =arregloPasatiempos.slice(1,3) //unicamente muestra los valores que contengan las posiciones señalas
+
+console.log(nuevosPasatiempos)
+
+arregloPasatiempos.splice(1,2, "programar", "dormir") /*elimina los valores contenidos entre a partir de las posiciones indicadas, 
+y al agregar " " añade algun valor en las posiciones señaladas*/
+
+console.log(arregloPasatiempos)
+
+const letters = ['b', 'P', 'd', 'a'];
+
+console.log(letters)
+
+letters.reverse() //los valores aparecen en reversa
+
+console.log(letters) // para poner una comilla es alt + 39 -> ''
+
+const letters1 = ['a', 'e', 'i'];
+const letters2 = ['o', 'u'];
+const letters3 = [1, 2];
+
+const lettersConcat = letters1.concat(letters2, letters3);//une los valores de los objetos que se indiquen
+
+console.log(lettersConcat)
+
