1d19021cc1c430afcabc9c57256d8075f065e26b
Rob <rob.mtzgu@gmail.com>
2024-10-30 19:22:40 -0600

    segundo

---
+++ clase6/main.js
@@ -0,0 +1,15 @@
+//una sola comilla alt + 39 -> ''
+//comilla inclinada alt + 96 -> ``
+
+// en el let, los "id" de HTML se escriben con camelCase 
+let imageBtn = document.getElementById("image-btn") //"document" forma en la que se accede a los elementos
+
+console.log(imageBtn)
+
+let containers = document.getElementsByClassName("container");
+
+console.log(containers) // se debe insertar el nombre del "let"
+
+let arrayContainers = Array.from(containers)
+
+console.log(containers)
